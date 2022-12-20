dd9f223f95d76fc15ad04dbb97c709e17aa98ed1
Geralda101 <geraldine.akunne@gmail.com>
2022-12-20 09:04:49 -0800

    second try

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 class Square:
-    """Defines a square"""
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
 
+        self.__size = size
+
     def area(self):
-        """Returns current square area"""
-        return self.__size**2
+        ar = self.__size * self.__size
+        return ar
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 class Square:
-    """Defines a square"""
     def __init__(self, size=0):
-        """Initialises the data"""
         self.size = size
-
-    def area(self):
-        """Returns current square area"""
-        return self.__size**2
 
     @property
     def size(self):
-        """Getter method"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method"""
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        a = self.__size * self.__size
+        return a
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pr
